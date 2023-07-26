93439a3a0fb1a84bd9b8bf579da562470f7ca8ca
mdahtotul <mdahtotul@gmail.com>
2023-07-26 22:16:30 +0600

    retrive data from mysql

---
+++ README.md
@@ -0,0 +1,9 @@
+# Current Dir
+
+Code With Mosh - The Ultimate Django Series (Part 1)\5.Django ORM
+
+# Last Tutorial
+
+5- Retrieving Objects
+
+# Re-check: lesson -
--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,20 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Product
 
 
 def page_init(request):
-    query_set = Product.objects.all()
+    try:
+        product = Product.objects.get(pk=0)
+
+        # query_set = Product.objects.all()
+
+        # for product in query_set:
+        #     print(product)
+    except ObjectDoesNotExist:
+        pass
+        # return HttpResponse("Product not found")
 
     return render(request, "hello.html", {"name": "Arif"})
 
