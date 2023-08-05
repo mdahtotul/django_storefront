cc2ac204948bfccbc28c0feefc064b2db35b5c8e
MD ARIFUL HASAN <77568259+mdahtotul@users.noreply.github.com>
2023-08-05 20:34:21 +0600

    part 2-Building RESTful apis-tutorial 12 Model serializers important

--- store/serializers.py
+++ store/serializers.py
@@ -3,23 +3,40 @@ from decimal import Decimal
 from store.models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ["id", "title"]
 
 
-class ProductsSerializers(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class ProductsSerializers(serializers.ModelSerializer):
     price = serializers.DecimalField(
         max_digits=6, decimal_places=2, source="unit_price"
     )
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-    # collection = CollectionSerializer() #return collection object {"id": 1, "title": "Summer collection"}
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name="collection_detail",
-    )
+
+    collection = CollectionSerializer()
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.15)
+
+    class Meta:
+        model = Product
+        fields = ["id", "title", "price", "price_with_tax", "collection"]
+
+
+# class ProductsSerializers(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     price = serializers.DecimalField(
+#         max_digits=6, decimal_places=2, source="unit_price"
+#     )
+#     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+#     # collection = CollectionSerializer() #return collection object {"id": 1, "title": "Summer collection"}
+#     collection = serializers.HyperlinkedRelatedField(
+#         queryset=Collection.objects.all(),
+#         view_name="collection_detail",
+#     )
+
+#     def calculate_tax(self, product: Product):
+#         return product.unit_price * Decimal(1.15)
